7ce4aea0d4d1221c8702eceb4ecf6abfbdd7cabe
atsyatkovskiy <zenit-alex@ya.ru>
2020-11-02 20:29:56 +0300

    add tests: - test_admin_page_add_product; - test_admin_page_delete_product.

--- lesson8/tests/test_page_admin.py
+++ lesson8/tests/test_page_admin.py
@@ -120,3 +120,80 @@ def test_admin_page_delete_categories(browser, wait, login):
     assert alert_success == "Success: You have modified categories!\n×"
 
 
+# Создаем новый товар
+# localhost/admin/
+def test_admin_page_add_product(browser, wait, login):
+    """
+    1) Клик Catalog -> Products и переход на страницу Products
+    """
+    browser.find_element(By.CSS_SELECTOR, "#menu-catalog").click()
+    el_cat = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#collapse1 > li:nth-child(2) > a")))
+    assert el_cat.get_property("innerText") == "Products"
+    el_cat.click()
+    """
+    2) На странице Products жмём на кнопку "Add new"
+    """
+    page_header = browser.find_element(By.CSS_SELECTOR, "#content > div.page-header > div > h1")
+    assert page_header.text == "Products"
+    button_add_new = browser.find_element(By.CSS_SELECTOR, "a.btn.btn-primary")
+    button_add_new.click()
+    """
+    3) На странице Products, раздел Add Product заполняем поля:
+    General -> Product Name:   Apple_product_name
+    General -> Meta Tag Title: Apple_tag_title
+    Data ->    Model: Apple_model
+    """
+    input_name_product = browser.find_element(By.CSS_SELECTOR, "#input-name1")
+    input_name_product.send_keys("Apple_product_name")
+    input_tag_title = browser.find_element(By.CSS_SELECTOR, "#input-meta-title1")
+    input_tag_title.send_keys("Apple_tag_title")
+    link_data = browser.find_element_by_link_text("Data")
+    link_data.click()
+    input_model = browser.find_element(By.CSS_SELECTOR, "#input-model")
+    input_model.send_keys("Apple_model")
+    """
+    4) Жмем на кнопку Save и проверяем, появился ли alert_success:
+    """
+    button_save = browser.find_element(By.CSS_SELECTOR, "div.pull-right > button.btn.btn-primary")
+    button_save.click()
+    alert_success = browser.find_element(By.CSS_SELECTOR, "div.alert.alert-success.alert-dismissible").text
+    assert alert_success == "Success: You have modified products!\n×"
+
+
+# Удаляем ранее созданную категорию Apple_product_name
+# localhost/admin/
+def test_admin_page_delete_product(browser, wait, login):
+    """
+    1) Клик Catalog -> Products и переход на страницу Products
+    """
+    browser.find_element(By.CSS_SELECTOR, "#menu-catalog").click()
+    el_cat = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#collapse1 > li:nth-child(2) > a")))
+    assert el_cat.get_property("innerText") == "Products"
+    el_cat.click()
+
+    """
+    2) Проверяем, есть ли в таблице новый товар Apple_product_name 
+    """
+    for i in range(0, 20):
+        i += 1
+        text_tr = browser.find_element(By.CSS_SELECTOR, f'table > tbody > tr:nth-child({i})').text
+        if text_tr == "Apple_product_name Apple_model $0.00 1 Enabled":
+            check = browser.find_element(By.CSS_SELECTOR,
+                                         f'table > tbody > tr:nth-child({i}) > td.text-center > input[type=checkbox]')
+            check.click()
+            break
+        else:
+            print(f"Элемент {text_tr} не найден")
+    """
+    3) Жмем на кнопку удалить
+    """
+    button_del = browser.find_element(By.CSS_SELECTOR, "#content > div.page-header > div > div > button.btn.btn-danger")
+    button_del.click()
+    """
+    4) В alert жмем на кнопку OK и проверяем, появился ли alert_success
+    """
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    alert_success = browser.find_element(By.CSS_SELECTOR, "div.alert.alert-success.alert-dismissible").text
+    assert alert_success == "Success: You have modified products!\n×"
+
